0589ae55c245c39b1868064a20849d1517b8aa1a
sklvv <iisokolov1@gmail.com>
2022-10-29 16:21:09 +0500

    chore: create reuse components

---
+++ src/shared/ui/Select.tsx
@@ -0,0 +1,74 @@
+import { Component, children } from "solid-js";
+
+export const SelectItem: Component<{
+  text: string;
+  handleActive: (value: boolean) => boolean;
+  handleValue: (value: string) => string;
+}> = (props) => {
+  return (
+    <span
+      class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-50 focus:outline-none"
+      role="menuitem"
+      tabindex="-1"
+      id="menu-item-0"
+      onClick={() => {
+        props.handleValue(`${props.text}`);
+        props.handleActive(false);
+      }}
+    >
+      {props.text}
+    </span>
+  );
+};
+
+export const Select: Component<{
+  title: string;
+  children: any;
+  isActive: boolean;
+  handleActive: (value: boolean) => boolean;
+}> = (props) => {
+  const handleClick = () => {
+    props.handleActive(!props.isActive);
+  };
+  return (
+    <div class="relative inline-block text-left">
+      <div onclick={handleClick}>
+        <button
+          type="button"
+          class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none"
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+        >
+          {props.title}
+          <svg
+            class="-mr-1 ml-2 h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+      {props.isActive && (
+        <div
+          class="transition ease-out duration-100 absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none cursor-pointer"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="menu-button"
+          tabindex="-1"
+        >
+          <div class="py-1" role="none">
+            {children(() => props.children)}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
