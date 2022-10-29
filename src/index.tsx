1925b78b9c0fa352b03063042629092db38e3cc8
sklvv <iisokolov1@gmail.com>
2022-10-29 16:22:08 +0500

    chore: move global styles to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 
-import "./index.css";
+import "./app/index.css";
 import App from "./app/App";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
